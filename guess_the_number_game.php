2c63d152d5eb4b90ced75822f9bc6e2aaf08d497
harufumi03 <flat.wa.chef@gmail.com>
2024-04-15 23:03:18 +0900

    処理を追加

--- guess_the_number_game.php
+++ guess_the_number_game.php
@@ -1,8 +1,17 @@
 <?php
-echo 'Please guess the number' . PHP_EOL;
+echo '数字を入力してください' . PHP_EOL;
+$min_number = (int)trim(fgets(STDIN));
+echo 'もう一つ数字を入力してください' . PHP_EOL;
+$max_number = (int)trim(fgets(STDIN));
 
-function guess_number(){
-  $rand_num = rand(5, 15);
+function game_start($min_number, $max_number){
+  echo 'Please guess the number' . PHP_EOL;
+  echo guess_number($min_number, $max_number);
+}
+
+
+function guess_number($min_number, $max_number){
+  $rand_num = rand($min_number, $max_number);
   for($i = 0; $i < 5; $i ++) {
     echo "challenge" . "[" . ($i + 1) . "] :";
     $number = (int)trim(fgets(STDIN));
@@ -10,10 +19,10 @@ function guess_number(){
     if($number == $rand_num) {
       return $number . " is correct!";
     } else {
-      echo "Your number is incorrect answer.";
+      echo "Your number is incorrect answer." . PHP_EOL;
     }
   }
   return "Sorry, you didn't guess the number. It was $rand_num";
 }
 
-echo guess_number();
+game_start($min_number, $max_number);
